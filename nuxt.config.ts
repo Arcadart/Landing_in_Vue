5fbd21292492c631d77d65bd5f7e198baebccfdd
Arcadart <gabonoesgay@gmail.com>
2024-05-05 21:33:19 -0600

    Se agrega Formkit y acomodo de pagina y layouts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ['@formkit/nuxt'],
+  formkit: {
+    // Experimental support for auto loading (see note):
+    autoImport: true,
+    configFile: './formkit.config.ts',
+  }
 })
