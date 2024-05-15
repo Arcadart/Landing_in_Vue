df4b08d13900530b88beb1ad7f13c627256732d4
Arcadart <gabonoesgay@gmail.com>
2024-05-14 21:18:16 -0600

    Se crea login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,70 @@
+<script setup>
+  // This is just a mock of an actual axios instance.
+  const axios = {
+    post: () => new Promise(r => setTimeout(r, 1000))
+  }
+
+  async function login (credentials) {
+    const res = await axios.post(credentials)
+    // do some login things now
+    alert('Logged in!')
+  }
+</script>
+
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+<div>
+  <div class="container-fluid">
+    <div class="row vh-100">
+      <div class="col-7 welcome d-flex align-items-center ">
+        <h1 class="text-welcome mb-5 pb-5 pl-5">
+          Welcome!
+        </h1>
+      </div>
+      <div class="col-5 d-flex justify-content-center align-items-center">
+        <div class="row row-cols-3">
+          <div class="d-flex justify-content-center align-items-center col-12 mb-5">
+            <div class="logo row d-flex justify-content-center">
+              <img src="/assets/img/logo.svg" alt="" class="imag col-12">
+              <h1 class="d-flex justify-content-center align-items-center col-12">To Do List</h1>
+            </div>
+          </div>
+
+          <div class="col-12 d-flex justify-content-center">
+            <div class="formulario d-flex justify-content-center">
+                <FormKit
+                type="form"
+                submit-label="Login"
+                @submit="login"
+                >
+                <FormKit
+                  name="email"
+                  label="Email address"
+                  validation="required|email"
+                />
+                <FormKit
+                  type="password"
+                  name="password"
+                  label="Password"
+                  validation="required"
+                />
+              </FormKit>
+            </div>
+          </div>
+          <div class="col-12 d-flex justify-content-center">
+            <p class="olvidaste"><a href="#">¿Olvidaste tu contraseña?</a></p>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
+
   </div>
+</div>
 </template>
+
+<script>
+// Use a static import for server-side compatibility
+import '~/assets/styles/first.css'
+
+// Caution: Dynamic imports are not server-side compatible
+import('~/assets/styles/first.css')
+</script>
