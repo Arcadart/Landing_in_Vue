8e1c537e9146f5602233573543d0a9437a6e034c
David Muñoz Guzmán <davemg71@hotmail.com>
2024-05-25 20:03:34 -0600

    home page in progress

---
+++ pages/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2">
+        <div>
+          <span>Logo</span>
+          <div class="d-flex align-items-center gap-2">
+            <div
+              class="overflow-hidden"
+              style="width: 40px; height: 40px; border-radius: 50%"
+            >
+              <img
+                src="https://pbs.twimg.com/profile_images/1603791594762604549/RmkcXqLF_400x400.jpg"
+              />
+            </div>
+            <div class="d-flex flex-column">
+              <span>David Muñoz Guzmán</span>
+              <span class="text-danger small">Cerrar sesión</span>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between w-100">
+            <span>Categories</span>
+            <span>Icono</span>
+          </div>
+          <div class="d-flex flex-column gap-2">
+            <div><span>Tag title</span></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-10">b</div>
+    </div>
+  </div>
+</template>
